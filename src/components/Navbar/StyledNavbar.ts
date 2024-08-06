d83003c1f10367d14a9761f7d4e09d7dc1a0f309
Dvl <84147332+Sylbird@users.noreply.github.com>
2024-08-06 18:35:01 -0500

    Added Sidebar in addition to Navbar

--- src/components/Navbar/StyledNavbar.ts
+++ src/components/Navbar/StyledNavbar.ts
@@ -2,26 +2,29 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const StyledNavbar = styled.nav`
+  align-items: center;
   background-color: rgba(37, 37, 68, 1);
   display: flex;
-  width: 100%;
-
-  ul {
-    align-items: center;
-    display: flex;
-    height: 50px;
-    justify-content: center;
-    list-style: none;
-    width: inherit;
-  }
+  height: 50px;
+  justify-content: space-between;
+  padding: 0 0.8rem;
+  position: sticky;
+  top: 0;
+  width: calc(100% - 1.6rem);
+  z-index: 101;
 `;
 
 export const StyledNavLink = styled(NavLink)`
+  align-items: center;
+  border-radius: 5px;
   color: rgba(255, 255, 255, 1);
+  display: flex;
   font-weight: 500;
-  padding: 0.5rem 0.5rem 0.5rem 0.5rem;
+  gap: 5px;
+  height: 36px;
+  padding: 0 8px;
   text-decoration: none;
-  transition: 0.2s ease-in-out;
+  transition: background-color 0.2s ease-in-out;
   user-select: none;
 
   &:hover {
@@ -30,4 +33,49 @@ export const StyledNavLink = styled(NavLink)`
   &.active {
     background-color: rgba(49, 120, 198, 1);
   }
+`;
+
+type StyledSidebarProps = {
+  $open: boolean;
+};
+
+export const StyledSidebar = styled.div<StyledSidebarProps>`
+  background-color: rgba(37, 37, 68, 1);
+  height: calc(100vh - 50px);
+  min-width: 240px;
+  max-width: 315px;
+  position: fixed;
+  right: 0px;
+  top: 50px;
+  transition: translate 0.3s ease-in-out;
+  translate: ${({ $open }) => ($open ? 'none' : '100%')};
+  z-index: 101;
+
+  ul {
+    list-style: none;
+  }
+`;
+
+export const ToggleSidebar = styled.button`
+  background-color: transparent;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  height: 36px;
+  transition: background-color 0.2s ease-in-out;
+  width: 36px;
+
+  &:hover {
+    background-color: rgba(20, 180, 215, 1);
+  }
+`;
+
+export const SidebarBackdrop = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0px;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 100;
 `;
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,30 +1,74 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import {
+  SidebarBackdrop,
   StyledNavbar,
-  StyledNavLink
+  StyledNavLink,
+  StyledSidebar,
+  ToggleSidebar
 } from 'src/components/Navbar/StyledNavbar';
 
 const NavBar: FC = () => {
+  const [sidebar, setSidebar] = useState<boolean>(false);
+  const toggleSidebar = () => setSidebar(!sidebar);
+
   return (
-    <StyledNavbar>
-      <ul>
+    <>
+      <StyledNavbar>
         <StyledNavLink to={`/`} end>
-          Home
+          <svg
+            fill="rgba(255, 255, 255, 1)"
+            height="20px"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="5 6.02 22 19.98"
+          >
+            <path d="M27 18.039 16 9.501 5 18.039V14.56l11-8.54 11 8.538v3.481zm-2.75-.31v8.251h-5.5v-5.5h-5.5v5.5h-5.5v-8.25L16 11.543l8.25 6.186z"></path>{' '}
+          </svg>
+          <span>Home</span>
         </StyledNavLink>
-        <StyledNavLink to={`/Clock`} end>
-          Clock
-        </StyledNavLink>
-        <StyledNavLink to={`/Counter`} end>
-          Counter
-        </StyledNavLink>
-        <StyledNavLink to={`/ToDo`} end>
-          ToDoApp
-        </StyledNavLink>
-        <StyledNavLink to={`/AI`} end>
-          A.I
-        </StyledNavLink>
-      </ul>
-    </StyledNavbar>
+        <ToggleSidebar onClick={toggleSidebar} aria-label="toggle sidebar">
+          {sidebar ? (
+            <svg
+              fill="#fff"
+              height="20px"
+              viewBox="0 0 24 24"
+              width="20px"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M20.746 3.329a1 1 0 0 0-1.415 0l-7.294 7.294-7.294-7.294a1 1 0 1 0-1.414 1.414l7.294 7.294-7.294 7.294a1 1 0 0 0 1.414 1.415l7.294-7.295 7.294 7.295a1 1 0 0 0 1.415-1.415l-7.295-7.294 7.295-7.294a1 1 0 0 0 0-1.414Z" />
+            </svg>
+          ) : (
+            <svg
+              fill="#fff"
+              height="20px"
+              viewBox="0 0 16 16"
+              width="20px"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M1 2.75A.75.75 0 0 1 1.75 2h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 2.75Zm0 10a.75.75 0 0 1 .75-.75h12.5a.75.75 0 0 1 0 1.5H1.75a.75.75 0 0 1-.75-.75ZM1.75 7a.75.75 0 0 0 0 1.5h12.5a.75.75 0 0 0 0-1.5H1.75Z"></path>
+            </svg>
+          )}
+        </ToggleSidebar>
+      </StyledNavbar>
+      <StyledSidebar $open={sidebar}>
+        <ul>
+          <li>
+            <StyledNavLink to={`/Clock`} end>
+              Clock
+            </StyledNavLink>
+            <StyledNavLink to={`/Counter`} end>
+              Counter
+            </StyledNavLink>
+            <StyledNavLink to={`/ToDo`} end>
+              ToDoApp
+            </StyledNavLink>
+            <StyledNavLink to={`/AI`} end>
+              A.I
+            </StyledNavLink>
+          </li>
+        </ul>
+      </StyledSidebar>
+      {sidebar && <SidebarBackdrop onClick={toggleSidebar} />}
+    </>
   );
 };
 
